14825c75b931ef18c71f429d3432b265fde5aebe
Benjamin Eskola <ben@eskola.uk>
2022-11-19 10:54:28 +0000

    Add basic views for books

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
-  factory :author do
+  factory :author, aliases: [:first_author] do
     forenames { Faker::Name.first_name + " " + Faker::Name.middle_name }
     surname { Faker::Name.last_name }
   end
 
   factory :book do
-    title { Faker::Faker::Book.title }
+    title { Faker::Book.title }
+    first_author
   end
 end
